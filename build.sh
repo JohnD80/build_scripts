1a240cd9e0469813ef8b234c2d37fd2bf22748ea
Vojtěch Hejsek <hejsekvojtech@gmail.com>
2017-06-20 21:18:49 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
+# This will set device model and TWRP version you are building
 export device="Z500"
 export twrpver="3.1.1-0"
 
-echo $device
-
+# Main building script
 make clean
 . build/envsetup.sh
 lunch omni_$device-eng
 make -j64 recoveryimage
 cd out/target/product/$device
 mv recovery.img twrp-$twrpver-$device.img
+
+# Uploading to Google Drive
 gdrive upload twrp-$twrpver-$device.img
 cd ../../../..
