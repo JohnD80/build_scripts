8c365abf4e0eb45329dcffd4a120669ec3e089f8
Vojtěch Hejsek <hejsekvojtech@gmail.com>
2017-06-21 17:01:39 +0200

    Update gdrive_install.sh

--- gdrive_install.sh
+++ gdrive_install.sh
@@ -4,6 +4,7 @@
 
 cd ~
 wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download
+reset
 mv uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE gdrive
 chmod +x gdrive
 sudo install gdrive /usr/local/bin/gdrive
