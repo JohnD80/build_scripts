68d90b64661960eec6a9b97e92b94ee0c14e660c
Vojtěch Hejsek <hejsekvojtech@gmail.com>
2017-06-27 22:39:30 +0200

    Z500: use MEGA instead of Google Drive

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 # Before building, please edit these script settings to suit your device
-export devicetree="https://github.com/hejsekvojtech/android_device_acer_Z500.git"
 export brand="acer"
 export device="Z500"
 export twrpver="3.1.1-0"
 export branch="android-5.1"
 
 # Clonning device tree
-git clone $devicetree -b $branch device/$brand/$device
+git clone https://github.com/hejsekvojtech/android_device_$brand_$device.git -b $branch device/$brand/$device
 
 # Main building script
 . build/envsetup.sh
@@ -18,7 +17,12 @@ cd out/target/product/$device
 mv recovery.img twrp-$twrpver-$device.img
 
 # Uploading to Google Drive
-gdrive upload twrp-$twrpver-$device.img
+# gdrive upload twrp-$twrpver-$device.img
+# cd ../../../..
+
+# Uploading to MEGA
+megarm /Root/LPAD/Devices/$device/Recovery/twrp-$twrpver-$device.img
+megaput --no-progress --path /Root/LPAD/Devices/$device/Recovery twrp-$twrpver-$device.img
 cd ../../../..
 
 # Cleaning the source
@@ -26,4 +30,4 @@ make clean
 cd device
 rm -rf $brand
 cd ..
-echo "twrp-$twrpver-$device.img has been built successfuly!"
+echo "twrp-$twrpver-$device.img has been built and uploaded successfuly!"
